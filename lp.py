5dc1cdcf57cae31e4ee4ccbaf30e1464011cc5ec
18PatZ <18patz@students.harker.org>
2023-03-10 01:01:08 -0600

    Execution cost evaluation for optimal checkin policy

--- lp.py
+++ lp.py
@@ -10,16 +10,23 @@ import time
 #   argument checker off: 7.3
 # -> 58% faster
 
-def makeConstraint(mdp, discount, lp, v, state, action):
-    a1 = [v[end_state] for end_state in mdp.transitions[state][action].keys()]
+def makeConstraint(mdp, discount, lp, v, state, action, is_negative):
+    if not is_negative:
+        a1 = [v[end_state] for end_state in mdp.transitions[state][action].keys()]
+    else:
+        a1 = [-v[end_state] for end_state in mdp.transitions[state][action].keys()]
     a2 = [mdp.transitions[state][action][end_state] for end_state in mdp.transitions[state][action].keys()]
     leSum = lp.scal_prod(a1, a2)
-    return v[state] >= (mdp.rewards[state][action] + discount * leSum)
 
-def makeConstraintsList(mdp, discount, lp, v, restricted_action_set):
-    return [makeConstraint(mdp, discount, lp, v, state, action) for state in mdp.states for action in (mdp.actions if restricted_action_set is None else restricted_action_set[state]) if action in mdp.transitions[state]]
+    if not is_negative:
+        return v[state] >= (mdp.rewards[state][action] + discount * leSum)
+    else:
+        return v[state] <= -(mdp.rewards[state][action] + discount * leSum)
 
-def linearProgrammingSolve(grid, mdp, discount, restricted_action_set = None):
+def makeConstraintsList(mdp, discount, lp, v, restricted_action_set, is_negative):
+    return [makeConstraint(mdp, discount, lp, v, state, action, is_negative) for state in mdp.states for action in (mdp.actions if restricted_action_set is None else restricted_action_set[state]) if action in mdp.transitions[state]]
+
+def linearProgrammingSolve(grid, mdp, discount, restricted_action_set = None, is_negative = False):
 
     time_start = time.time()
 
@@ -27,9 +34,13 @@ def linearProgrammingSolve(grid, mdp, discount, restricted_action_set = None):
     v = lp.continuous_var_dict(keys = mdp.states, name = "v")
 
     lp.v_sum = lp.sum_vars(v)
-    objective = lp.minimize(lp.v_sum)
 
-    lp.add_constraints(makeConstraintsList(mdp, discount, lp, v, restricted_action_set))
+    if not is_negative:
+        objective = lp.minimize(lp.v_sum)
+    else:
+        objective = lp.maximize(lp.v_sum)
+
+    lp.add_constraints(makeConstraintsList(mdp, discount, lp, v, restricted_action_set, is_negative))
     
     time_elapsed = (time.time() - time_start)
          
@@ -52,6 +63,8 @@ def linearProgrammingSolve(grid, mdp, discount, restricted_action_set = None):
     # print(lp.solution.get_value_dict(v))
 
     values = lp.solution.get_value_dict(v)
+    if is_negative:
+        values = {state: -values[state] for state in values}
 
     policy = {}
     for state in mdp.states:
